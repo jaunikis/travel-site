f9d186ce3958fa2c8c7deaa6356e27391b6614f0
jaunikis <jaunikis@gmail.com>
2017-05-07 15:10:49 +0100

    header finished

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -108,6 +108,7 @@ var MobileMenu = function () {
 			console.log('was clicked');
 			this.menuContent.toggleClass("site-header__menu-content--is-visible");
 			this.siteHeader.toggleClass("site-header--is-expanded");
+			this.menuIcon.toggleClass("site-header__menu-icon--close-x");
 		}
 	}]);
 
